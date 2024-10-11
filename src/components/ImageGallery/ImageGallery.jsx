4b10e59485cd904f5d6f51a65118a8c9559e64f1
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-12 02:00:42 +0300

    Add ImageGallery markup

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,18 @@
 import ImageCard from '../ImageCard/ImageCard';
 import css from './ImageGallery.module.css';
-import fetchImages from '../../unsplash-api';
 
-const ImageGallery = () => {
+const ImageGallery = ({ images }) => {
+  if (images.length === 0) {
+    return null;
+  }
   return (
-    <div className={css.list}>
-      <ImageCard />
-      <ImageCard />
-      <ImageCard />
-    </div>
+    <ul className={css.gallery}>
+      {images.map(image => (
+        <li key={image.id} className={css.item}>
+          <ImageCard src={image.urls.small} alt={image.alt_description} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
