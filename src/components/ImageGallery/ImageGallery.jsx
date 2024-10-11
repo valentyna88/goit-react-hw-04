36dc667b17bde55453f90c18c6ff2ff6271e8bb4
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-12 02:12:22 +0300

    Add ImageCard and ImageGallery styles

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,9 +2,9 @@ import ImageCard from '../ImageCard/ImageCard';
 import css from './ImageGallery.module.css';
 
 const ImageGallery = ({ images }) => {
-  if (images.length === 0) {
-    return null;
-  }
+  // if (images.length === 0) {
+  //   return null;
+  // }
   return (
     <ul className={css.gallery}>
       {images.map(image => (
