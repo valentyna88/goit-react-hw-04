58adb8b1ce4a30bfeadad119b4a74b66e135344d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-11 23:23:41 +0300

    Update unsplash-api

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,11 @@
 import { GrSearch } from 'react-icons/gr';
 import css from './SearchBar.module.css';
+// import toast from 'react-hot-toast';
 
 const SearchBar = () => {
   return (
     <header className={css.header}>
       <form className={css.form}>
-        <GrSearch />
         <input
           className={css.field}
           type="text"
@@ -14,6 +14,9 @@ const SearchBar = () => {
           autoFocus
           placeholder="Search images and photos"
         />
+        <button className={css.searchButton} type="button">
+          <GrSearch />
+        </button>
       </form>
     </header>
   );
--- src/unsplash-api.js
+++ src/unsplash-api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-const API_KEY = '';
-const BASE_URL = 'https://api.unsplash.com/search/photos';
+const API_KEY = 'qSg347jVb1hWhhBVkTz2gPP9EKim76v11DHuIY7tt8M';
+axios.defaults.baseURL = 'https://api.unsplash.com/';
 
 const fetchImages = async (query, page) => {
   try {
-    const response = await axios.get(BASE_URL, {
+    const response = await axios.get('/search/photos', {
       params: {
-        query: query,
-        page: page,
+        query,
+        page,
         per_page: 10,
         client_id: API_KEY,
       },
     });
     return response.data;
   } catch (error) {
-    console.error('Error fetching images:', error);
+    console.error('Error fetching images:', error.message);
     throw error;
   }
 };
